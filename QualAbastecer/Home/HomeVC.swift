8f83338e4a773570c87961ef5577463332f30b6a
personal <filipe.boeck@hotmail.com>
2022-12-01 18:16:34 -0300

    ajuste da imagem de background do app

--- QualAbastecer/Home/HomeVC.swift
+++ QualAbastecer/Home/HomeVC.swift
@@ -1,11 +1,17 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    var screen: HomeScreen?
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
     }
     
 }
