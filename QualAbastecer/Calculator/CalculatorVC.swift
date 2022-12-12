633b9d9e126bf673c33e46cf9eac90d5d197ff5d
personal <filipe.boeck@hotmail.com>
2022-12-12 10:57:23 -0300

    trabalhando na tratativa dos textsfield dos combustiveis

--- QualAbastecer/Calculator/CalculatorVC.swift
+++ QualAbastecer/Calculator/CalculatorVC.swift
@@ -18,11 +18,24 @@ class CalculatorVC: UIViewController {
 extension CalculatorVC: CalculatorScreenDelegate {
     
     func tappedCalculateButton() {
-        print(#function)
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0")
+                                    as? Double) ?? 0.0
+        
+        let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0")
+                                    as? Double) ?? 0.0
+        
+        if ethanolPrice / gasPrice > 0.7 {
+            print("melhor usar gasolina")
+        } else {
+            print("melhor usar alcool")
+        }
     }
     
     func tappedBackButton() {
-        print(#function)
+        navigationController?.popViewController(animated: true)
     }
     
 }
