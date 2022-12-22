3cec238b784ac35c7aa835914ef3be72f1d85e0c
Filipe Wolf Boeck <filipewolfboeck@MacBook-Air-de-Angela.local>
2022-12-22 11:40:44 -0300

    tratativa dos alerts referente a textFields vazios

--- QualAbastecer/Calculator/CalculatorVC.swift
+++ QualAbastecer/Calculator/CalculatorVC.swift
@@ -3,6 +3,7 @@ import UIKit
 class CalculatorVC: UIViewController {
     
     var screen: CalculatorScreen?
+    var alert: Alert?
     
     override func loadView() {
         screen = CalculatorScreen()
@@ -11,26 +12,44 @@ class CalculatorVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        alert = Alert(controller: self)
         screen?.delegate(delegate: self)
+    }
+    
+    func validateTextFields() -> Bool {
+        guard let ethanolPrice = screen?.ethanolPriceTextField.text else { return false }
+        guard let gasPrice = screen?.gasPriceTextField.text else { return false }
+        
+        if ethanolPrice.isEmpty && gasPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção!", message: "Informe os valores do álcool e da gasolina.")
+            return false
+        } else if ethanolPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção!", message: "Informe o valor do álcool.")
+            return false
+        } else if gasPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção!", message: "Informe o valor da gasolina.")
+            return false
+        }
+        return true
     }
 }
 
 extension CalculatorVC: CalculatorScreenDelegate {
-    
     func tappedCalculateButton() {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
         
-        let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0")
-                                    as? Double) ?? 0.0
-        
-        let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0")
-                                    as? Double) ?? 0.0
-        
-        if ethanolPrice / gasPrice > 0.7 {
-            print("melhor usar gasolina")
-        } else {
-            print("melhor usar alcool")
+        if validateTextFields() {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            
+            let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+            
+            let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+            
+            if ethanolPrice / gasPrice > 0.7 {
+                print("melhor usar gasolina")
+            } else {
+                print("melhor usar alcool")
+            }
         }
     }
     
