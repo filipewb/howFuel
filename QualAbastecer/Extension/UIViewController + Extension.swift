41a10a9765bb55e7a73b0835004b06481620890c
personal <filipe.boeck@hotmail.com>
2023-01-09 19:48:43 -0300

    abaixa teclado quando clica na view

---
+++ QualAbastecer/Extension/UIViewController + Extension.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
